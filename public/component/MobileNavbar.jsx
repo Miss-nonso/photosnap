a716c3f217346996540d19dcf75f476f8a133e00
Miss-nonso <chinonsodaniels@yahoo.com>
2025-01-23 20:40:05 +0100

    Feat: add animation to mobile nav

--- public/component/MobileNavbar.jsx
+++ public/component/MobileNavbar.jsx
@@ -1,10 +1,21 @@
+"use client";
 import Link from "next/link";
-
 import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
 
 const MobileNavbar = () => {
   return (
-    <div className="mobileNav">
+    <motion.div
+      className="mobileNav"
+      initial={{ opacity: 0, y: -1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 0.9,
+        // delay: 0.000,
+        // ease: "easeOut"
+        ease: "easeIn"
+      }}
+    >
       <div id="myLinks">
         <a href="/stories">STORIES</a>
         <a href="/features">FEATURES</a>
@@ -14,7 +25,7 @@ const MobileNavbar = () => {
       <div className="nav-invite-btn">
         <Button>Get an invite</Button>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
