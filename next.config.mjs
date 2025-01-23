8146f1a0db9b036d716d9fd09c80d101c012041a
Miss-nonso <chinonsodaniels@yahoo.com>
2025-01-23 08:54:13 +0100

    Feat: stories hero fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["vercel.app", "netlify.app"]
+  }
+};
 
 export default nextConfig;
